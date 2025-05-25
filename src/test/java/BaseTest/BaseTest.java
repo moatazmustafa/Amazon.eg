6c58501fe96e22b3fd1591de7d9ad7a20bc8b5ac
moataztest <moatazmustafa123@gmail.com>
2025-05-25 23:56:36 +0300

    incognito

--- src/test/java/BaseTest/BaseTest.java
+++ src/test/java/BaseTest/BaseTest.java
@@ -41,7 +41,7 @@ public abstract class BaseTest {
         // Avoid detection
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-        options.addArguments("--incognito");
+       // options.addArguments("--incognito");
 
         // Initialize driver
         driver = new ChromeDriver(options);
